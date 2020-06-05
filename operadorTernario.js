0ad10b22efd10e8e223f6af28ee5ac28e3c4a9db
Fernando Guedes <fernando.jobs@live.com>
2020-06-04 21:52:56 -0300

    estudo de operador unario e operador ternario

---
+++ operadorTernario.js
@@ -0,0 +1,8 @@
+
+// Operador Ternário
+const resultado = (nota) => {
+    return nota >= 7 ? 'Aprovado' : 'Reprovado'
+}
+
+console.log(resultado(7.1))
+console.log(resultado(5))
---
+++ operadorUnario.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+//pós fixado
+num1++
+console.log(num1)
+
+//pré fixado
+--num1
+console.log(num1)
